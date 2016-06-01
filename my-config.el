ceea070689c350145a41e6dfd7cc8fa554d134a2
Phil Windley <phil@windley.org>
2016-06-01 10:13:39 -0600

    congfigure backups

--- my-config.el
+++ my-config.el
@@ -47,3 +47,12 @@
 (require 'mouse-copy)
 (global-set-key [C-down-mouse-1] 'mouse-drag-secondary-pasting)
 (global-set-key [C-S-down-mouse-1] 'mouse-drag-secondary-moving)
+
+;;; backups
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist '(("." . "~/.saves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)  
